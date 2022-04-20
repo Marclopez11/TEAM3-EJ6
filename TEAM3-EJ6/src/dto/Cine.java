4d169c6300db97b44d16cd95e1e3b449fc73de9d
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-20 22:58:18 +0200

    "Clase Cine"

--- TEAM3-EJ6/src/dto/Cine.java
+++ TEAM3-EJ6/src/dto/Cine.java
@@ -1,5 +1,37 @@
 package dto;
 
 public class Cine {
+	
+	private int precioEntrada = 7;
+		
+	
+	public Cine(int precioEntrada) {
+			super();
+			this.precioEntrada = precioEntrada;
+		}
 
+	public int getPrecioEntrada() {
+		return precioEntrada;
+	}
+
+
+	public void setPrecioEntrada(int precioEntrada) {
+		this.precioEntrada = precioEntrada;
+	}
+
+
+	public void mostrarAsientos() {
+
+	int[] filas = {1, 2, 3, 4, 5, 6, 7, 8};
+	String[] columnas = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+	
+		for(int i=0; i<filas.length; i++) {
+			for (int j=0; j<columnas.length; j++) {
+				if(columnas[j] == "I") {
+					System.out.println("");
+				}
+			System.out.print(filas[i]+columnas[j]+" ");
+			}
+		}
+	}
 }
--- TEAM3-EJ6/src/main/App.java
+++ TEAM3-EJ6/src/main/App.java
@@ -1,10 +1,14 @@
 package main;
 
-public class App {
+import dto.Cine;
+
+public class App{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Cine cine = new Cine(7);
+		cine.mostrarAsientos();
 	}
 
 }
