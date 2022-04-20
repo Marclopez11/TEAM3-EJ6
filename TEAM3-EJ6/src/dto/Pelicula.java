96ed62c171a0eb9c53b38650755fe85baf9ffe7e
Marc Lopez <marclopezmarco469@gmail.com>
2022-04-20 22:07:50 +0200

    Clase Pelicula

--- TEAM3-EJ6/src/dto/Pelicula.java
+++ TEAM3-EJ6/src/dto/Pelicula.java
@@ -1,5 +1,48 @@
 package dto;
 
 public class Pelicula {
+	
+	
+	String titulo;
+	int duracion;
+	int edadMinima;
+	String director;
+	public Pelicula(String titulo, int duracion, int edadMinima, String director) {
+		super();
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.edadMinima = edadMinima;
+		this.director = director;
+	}
+	
+
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public int getEdadMinima() {
+		return edadMinima;
+	}
+	public void setEdadMinima(int edadMinima) {
+		this.edadMinima = edadMinima;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	
+	
+	
+	
 
 }
