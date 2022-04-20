769f6ac2cf35471451d2b9375716c1dd7597556f
Marc Lopez <marclopezmarco469@gmail.com>
2022-04-20 23:23:14 +0200

    Arreglado Cine

--- TEAM3-EJ6/src/dto/Cine.java
+++ TEAM3-EJ6/src/dto/Cine.java
@@ -1,37 +1,48 @@
 package dto;
 
 public class Cine {
-	
+
 	private int precioEntrada = 7;
-		
 	
+	private int[] filas = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	private String[] columnas ={ "A-", "B-", "C-", "D-", "E-", "F-", "G-", "H-", "I-" };
+
 	public Cine(int precioEntrada) {
-			super();
-			this.precioEntrada = precioEntrada;
-		}
+		super();
+		this.precioEntrada = precioEntrada;
+	}
 
 	public int getPrecioEntrada() {
 		return precioEntrada;
 	}
 
-
 	public void setPrecioEntrada(int precioEntrada) {
 		this.precioEntrada = precioEntrada;
 	}
 
-
 	public void mostrarAsientos() {
-
-	int[] filas = {1, 2, 3, 4, 5, 6, 7, 8};
-	String[] columnas = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
-	
-		for(int i=0; i<filas.length; i++) {
-			for (int j=0; j<columnas.length; j++) {
-				if(columnas[j] == "I") {
+		
+		
+		
+		for (int i = 0; i < filas.length; i++) {
+			for (int j = 0; j < columnas.length; j++) {
+				if (columnas[j] == "A-") {
 					System.out.println("");
+
 				}
-			System.out.print(filas[i]+columnas[j]+" ");
+				System.out.print(filas[i] + columnas[j] + " ");
 			}
 		}
+
 	}
+	
+	
+	
+	
+	//comprobarDinero
+	//comprobarEdad
+	
+
+	
+
 }
